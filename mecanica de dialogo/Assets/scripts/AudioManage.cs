7e816b0922f4421850ab416e46c561bfa3c818a5
Estela7714 <s.stephany@escolar.ifrn.edu.br>
2026-03-30 08:34:15 -0300

    audioManager

---
+++ mecanica de dialogo/Assets/scripts/AudioManage.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+  // #region Singleton 
+   public static AudioManager Instance;
+
+   private void Awake()
+   {
+       if (Instance == null)
+      {
+         Instance = this;
+         DontDestroyOnLoad(gameObject);
+      }
+      else
+      {
+         Destroy(gameObject);
+      }
+   }
+
+   //endregion
+} 
+
